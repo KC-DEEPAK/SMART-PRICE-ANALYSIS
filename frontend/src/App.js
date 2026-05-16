32b3e3f757ff51cd3c00b8f361b981923443671a
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-05-16 21:00:19 +0530

    Testing app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-
-import {
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-} from "@clerk/clerk-react";
-
-import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
-import ComparisonPage from "./pages/ComparisonPage";
-import PriceListPage from "./pages/PriceListPage";
-import LoginPage from "./pages/LoginPage";
-import AccountPage from "./pages/AccountPage";
-import FertilizerPage from "./pages/FertilizerPage";
-import DiseaseFertilizerPage from "./pages/DiseaseFertilizerPage";
-import SeasonGuidePage from "./pages/SeasonGuidePage";
-import MapPage from "./pages/MapPage";
-import Chatbot from "./components/Chatbot";
-
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-
-        <SignedIn>
-          <Navbar />
-
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/price-list" element={<PriceListPage />} />
-            <Route path="/comparison" element={<ComparisonPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/account" element={<AccountPage />} />
-            <Route path="/fertilizer" element={<FertilizerPage />} />
-            <Route path="/season-guide" element={<SeasonGuidePage />} />
-            <Route path="/disease-guide" element={<DiseaseFertilizerPage />} />
-            <Route path="/map" element={<MapPage />} />
-          </Routes>
-
-          <Chatbot />
-        </SignedIn>
-
-        <SignedOut>
-          <RedirectToSignIn />
-        </SignedOut>
-
-      </BrowserRouter>
-    </AuthProvider>
+    <div>
+      <h1>SMART PRICE ANALYSIS</h1>
+    </div>
   );
 }
 
